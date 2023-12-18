95b64f24fa72ec3042d30d1eb9b0fc79f84a88b0
aidansoares23 <aidansoares@gmail.com>
2023-12-18 12:28:08 -0800

    fixed nav bar to link to other pages properly

--- src/components/pages/navbar/Navbar.js
+++ src/components/pages/navbar/Navbar.js
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Logo from "../../images/FinalLogo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function OurNavbar() {
+  const navigate = useNavigate();
+  const [expanded, setExpanded] = useState(false);
+
+  const closeMobileMenu = () => setExpanded(false);
+
+  const linkStyle = {
+    color: "#fff",
+    textDecoration: "none",
+    marginRight: "15px",
+    fontSize: "18px",
+    transition: "text-decoration 0.3s ease-in-out", 
+  };
+
+
   return (
-    <Navbar expand="lg" className="nav-container navbar-dark" style={{ backgroundColor: "#012D3D" }}>
+    <Navbar
+      expand="lg"
+      className="nav-container navbar-dark"
+      style={{ backgroundColor: "#012D3D" }}
+      expanded={expanded}
+    >
       <Container>
         <Link to="/">
           <Navbar.Brand>
             <img src={Logo} alt="Logo" className="nav-logo" />
           </Navbar.Brand>
         </Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setExpanded(!expanded)}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/" style={{ color: "#fff" }}>
-              Home
+            <Nav.Link onClick={() => { navigate("/"); closeMobileMenu(); }}>
+              <Link to="/" style={linkStyle}>Home</Link>
             </Nav.Link>
-            <Nav.Link href="/our-work" style={{ color: "#fff" }}>
-              Our Work
+            <Nav.Link onClick={() => { navigate("/our-work"); closeMobileMenu(); }}>
+              <Link to="/our-work" style={linkStyle}>Our Work</Link>
             </Nav.Link>
-            <Nav.Link href="/contact" style={{ color: "#fff" }}>
-              Contact
+            <Nav.Link onClick={() => { navigate("/contact"); closeMobileMenu(); }}>
+              <Link to="/contact" style={linkStyle}>Contact</Link>
             </Nav.Link>
-            <Nav.Link href="/services" style={{ color: "#fff" }}>
-              Services
+            <Nav.Link onClick={() => { navigate("/services"); closeMobileMenu(); }}>
+              <Link to="/services" style={linkStyle}>Services</Link>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
